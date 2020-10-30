09185294dd49f72b29faea8d19005b1dd2f5be71
KhangTT <tatuankhang141095@gmail.com>
2020-10-30 21:11:19 +0700

    KhangTT commit

---
+++ PRJ_PhieuDuyetGia_VLA/PRJ_PhieuDuyetGia_VLA/Entities/clsPhieuDuyetGia_ChiTiet.vb
@@ -0,0 +1,21 @@
+﻿Public Class clsPhieuDuyetGia_ChiTiet
+    Public ID As String
+    Public IDPhieuDuyetGia As String
+    Public SoDonHang As String
+    Public TenDonVi As String
+    Public TenSanPham As String
+    Public QuyCachSanPham As String
+    Public SoLuong As Integer
+    Public GiayRuot As String
+    Public GiayBia As String
+    Public LaiDinhMuc As Double
+    Public DonGiaTinh As Double
+    Public CreatedOn As Date
+    Public CreatedBy As Integer
+    Public CreatedName As String
+    Public EditedOn As Date
+    Public EditedBy As Integer
+    Public EditedName As String
+    Public TrangThai As Boolean
+    Public MaDonViSuDung As Integer
+End Class
