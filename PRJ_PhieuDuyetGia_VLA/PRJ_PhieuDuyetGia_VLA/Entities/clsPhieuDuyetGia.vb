31b28147836aecacb0f931596881a013c447dae6
khangtt <khangtt@huongvietgroup.com>
2020-10-30 17:51:21 +0700

    comit

---
+++ PRJ_PhieuDuyetGia_VLA/PRJ_PhieuDuyetGia_VLA/Entities/clsPhieuDuyetGia.vb
@@ -0,0 +1,14 @@
+﻿Public Class clsPhieuDuyetGia
+    Public ID As Integer
+    Public SoPhieu As String
+    Public TenPhieu As String
+    Public TrangThai As Boolean
+    Public CreatedOn As Date
+    Public CreatedName As String
+    Public CreatedBy As Integer
+    Public EditedOn As Date
+    Public EditedName As String
+    Public EditedBy As Integer
+    Public KyNhay As Boolean
+    Public TrangThaiDuyet As Boolean
+End Class
---
+++ PRJ_PhieuDuyetGia_VLA/PRJ_PhieuDuyetGia_VLA/frmPhieuDuyetGia_CapNhat.vb
@@ -0,0 +1,6 @@
+Public Class frmPhieuDuyetGia_CapNhat 
+
+    Private Sub btnDong_ItemClick(ByVal sender As System.Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles btnDong.ItemClick
+        Me.Close()
+    End Sub
+End Class
